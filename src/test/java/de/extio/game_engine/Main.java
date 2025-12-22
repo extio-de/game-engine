d06d8c9f0f8ff97d582a3fc87feb663561a8a27f
extio-de <github@extio.de>
2025-12-22 12:22:40 +0100

    Add Main class for game engine application entry point

--- src/test/java/de/extio/game_engine/Main.java
+++ src/test/java/de/extio/game_engine/Main.java
@@ -1,21 +1,9 @@
 package de.extio.game_engine;
 
-import java.util.function.Consumer;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Condition;
-import org.springframework.context.annotation.ConditionContext;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -24,7 +12,7 @@ public class Main {
 	
 	public static void main(final String[] args) {
 		System.setProperty("game-engine.env", "standalone");
-
+		
 		// final Logger rootLogger = Logger.getLogger("");
 		// rootLogger.setLevel(Level.ALL);
 		// final ConsoleHandler consoleHandler = new ConsoleHandler();
@@ -33,22 +21,11 @@ public class Main {
 		// logger.setLevel(Level.ALL);
 		// logger.setUseParentHandlers(false);
 		// logger.addHandler(consoleHandler);
-
+		
 		final SpringApplicationBuilder builder = new SpringApplicationBuilder(Main.class);
 		builder.headless(false);
 		builder.properties("logging.level.de.extio=TRACE");
 		builder.run(args);
 	}
 	
-	static class StandaloneEnvCondition implements Condition {
-		
-		public StandaloneEnvCondition() {
-		}
-
-		@Override
-		public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
-			final String env = System.getProperty("game-engine.env", "prod");
-			return "standalone".equalsIgnoreCase(env);
-		}
-	}
 }
